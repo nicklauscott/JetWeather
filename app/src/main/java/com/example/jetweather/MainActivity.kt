18ae1ccd052f2ebfe3795efedb22496c0733b741
Nicklauscott <nicklauscott@gmail.com>
2023-10-26 02:59:56 -0700

    Unable to instantiate application

---
+++ app/src/main/java/com/example/jetweather/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.jetweather
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.jetweather.navigation.WeatherNavigation
+import com.example.jetweather.ui.theme.JetWeatherTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            WeatherApp()
+        }
+    }
+}
+
+// Weather API: 3b3d2f41895d0d5fdfc0a407e7d5f03f
+
+@Composable
+fun WeatherApp() {
+    JetWeatherTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(
+            color = MaterialTheme.colorScheme.background,
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                WeatherNavigation()
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/jetweather/navigation/WeatherNavigation.kt
@@ -0,0 +1,17 @@
+package com.example.jetweather.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.jetweather.screens.WeatherSplashScreen
+
+@Composable
+fun WeatherNavigation() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = WeatherScreens.SplashScreen.name) {
+        composable(route = WeatherScreens.MainScreen.name) {
+            WeatherSplashScreen(navController = navController)
+        }
+    }
+}
